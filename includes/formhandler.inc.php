abf2e096b5508a54f5136f0cc3f932d1a1faa1b2
magnolia1de <saturmag@gmail.com>
2024-02-24 15:53:23 +0100

    first commit

---
+++ includes/formhandler.inc.php
@@ -0,0 +1,39 @@
+<?php
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $name = $_POST["name"];
+    $surname = $_POST["surname"];
+    $pwd = $_POST["password"];
+    $class = $_POST["class"];
+    $admin = isset($_POST["admin"]) ? $_POST["admin"] : false;
+    $teacher = isset($_POST["teacher"]) ? $_POST["teacher"] : false;
+    $nickname = $_POST["nickname"];
+
+    try {
+        require_once "dbh.inc.php";
+
+        $query = "INSERT INTO users (name, surname, pwd, class_id, admin, teacher, nickname) VALUES
+        (?, ?, ?, ?, ?, ?, ?);";
+
+        $class_id = "SELECT id FROM class_group WHERE name = $class;";
+
+        $class = $class_id;
+
+        $stmt = $pdo->prepare($query);
+
+        $stmt->execute([$name, $surname, $pwd, $class, $admin, $teacher, $nickname]);
+
+        $pdo = null;
+        $stmt = null;
+
+        header("Location: ../index.php");
+
+        die();
+    } catch (PDOException $e) {
+        die("Query failed: " . $e->getMessage());
+    }
+}
+else {
+    header("Location: ../index.php");
+    exit();
+}
---
+++ index.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="pl">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/reset.css">
+    <link rel="stylesheet" href="css/main.css">
+    <title>Admin</title>
+</head>
+<body>
+    
+    <h3>Tworzenie uczniów</h3>
+    <form action="includes/formhandler.inc.php" method="post">
+        <input type="text" name="name" placeholder="Imie">
+        <input type="text" name="surname" placeholder="Nazwisko">
+        <input type="password" name="password" placeholder="Hasło">
+        <select name="class" placeholder="class">
+            <?php
+                require_once "includes/dbh.inc.php";
+                $query = "SELECT * FROM class_group;";
+                $stmt = $pdo->query($query);
+                $classes = $stmt->fetchAll();
+                foreach ($classes as $class) {
+                    echo "<option value='" . $class["id"] . "'>" . $class["name"] . "</option>";
+                }
+            ?>
+        </select>
+        <input type="checkbox" name="admin" placeholder="admin" default="false">
+        <input type="checkbox" name="teacher" placeholder="teacher" default="false">
+        <input type="text" name="nickname" placeholder="Login">
+        <button type="submit" name="submit">Twórz</button>
+    </form>
+
+    <h3>Tworzenie klas</h3>
+    <form action="includes/formhandler.inc.php" method="post">
+        <input type="text" name="name" placeholder="NazwaKlasy">
+        <button type="submit" name="submit">Twórz</button>
+    </form>
+</body>
+</html>
